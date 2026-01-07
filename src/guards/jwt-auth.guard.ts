5d8d8a2718fbd057bb99130fabdede9062a8f3cd
dima217 <dimapikulik217@mail.ru>
2026-01-08 00:43:26 +0300

    chore: mongo implementation

--- src/guards/jwt-auth.guard.ts
+++ src/guards/jwt-auth.guard.ts
@@ -1,26 +1,37 @@
-import { Injectable, ExecutionContext } from '@nestjs/common';
-import { Reflector } from '@nestjs/core';
-import { AuthGuard } from '@nestjs/passport';
-import { IS_PUBLIC_KEY } from '../decorators/public.decorator';
+import { Injectable, ExecutionContext } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { AuthGuard } from "@nestjs/passport";
+import { IS_PUBLIC_KEY } from "../decorators/public.decorator";
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {
-	constructor(private reflector: Reflector) {
-		super();
-	}
+export class JwtAuthGuard extends AuthGuard("jwt") {
+  constructor(private reflector: Reflector) {
+    super();
+  }
 
-	canActivate(context: ExecutionContext) {
-		const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
-			context.getHandler(),
-			context.getClass(),
-		]);
+  canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
 
-		if (isPublic) {
-			return true;
-		}
+    if (isPublic) {
+      return true;
+    }
 
-		return super.canActivate(context);
-	}
+    // Allow Swagger endpoints without authentication
+    const request = context.switchToHttp().getRequest();
+    const path = request.url?.split("?")[0] || request.path;
+    if (
+      path &&
+      (path === "/api" ||
+        path.startsWith("/api/") ||
+        path.startsWith("/api-json") ||
+        path.startsWith("/api-yaml"))
+    ) {
+      return true;
+    }
+
+    return super.canActivate(context);
+  }
 }
-
-
